44b15076d6a47fcb8f0f49022b965770b54c57be
ilozuluchris <ilozuluchidiuso@gmail.com>
2018-07-01 00:44:55 +0100

    Displaying results of flight search in an html form, also good for sharing by the way

--- booking/urls.py
+++ booking/urls.py
@@ -1,4 +1,4 @@
-from .views import  process_flight, book_flight
+from .views import  book_flight
 
 from django.conf.urls import url
 
@@ -6,5 +6,4 @@ from django.conf.urls import url
 urlpatterns = [
     url('flight', book_flight, name='book_flight'),
     url('hotel', book_flight, name='book_hotel'),
-    url('process/flight', process_flight, name='process_flight_booking')
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render, redirect
-from django.views.generic.edit import FormView
+from django.shortcuts import render
 from django.conf import settings
-# Create your views here.
-import logging
+
 import requests
 
 from .forms import FlightBookingForm
@@ -13,29 +11,24 @@ flight_booking_search = "https://api.sandbox.amadeus.com/v1.2/flights/affiliate-
 def search_for_flights(apikey, **kwargs):
         parmaters = kwargs
         parmaters['apikey'] = apikey
-        print(parmaters)
-        #todo change currency to dollars here
+        parmaters['currency'] = "USD"  # since US Dollars is the most popular currency
         res = requests.get(flight_booking_search, params=parmaters).json()
-        print(res)
-        return  res
+        return res
+
 
 def book_flight(request):
     if request.GET.get('origin') is not None:
         form = FlightBookingForm(request.GET)
         if form.is_valid():
-            #todo pass to api then get apikey then render content, test if api needs data in string or python datatype,
-            # also look up bootstrap cars
-            #request.GET does not behave exactly like a dict, so it should not passed around in the application
+            # todo  look up bootstrap cars
+            # request.GET does not behave exactly like a dict, so it should not passed around in the application
             flight_form_dict = {}
-            for k,v in request.GET.items():
+            for k, v in request.GET.items():
                 flight_form_dict[k] = v
-            print(flight_form_dict)
-            search_for_flights(settings.AMADEUS_API_KEY, **flight_form_dict)
+            # todo prepare function that serializes  info, dont forget to use that empty tag
+            flight_results = search_for_flights(settings.AMADEUS_API_KEY, **flight_form_dict)
+            # return redirect('show_flights_search_results', b=json.dumps(flight_results))
+            return render(request, 'process_flight.html', {'results': flight_results})
     else:
-        form=FlightBookingForm()
+        form = FlightBookingForm()
     return render(request, 'book_flight.html', {'form': form})
-
-
-def process_flight(request):
-    print('processing flight')
-    return render(request,'process_flight.html', {'post', request})
