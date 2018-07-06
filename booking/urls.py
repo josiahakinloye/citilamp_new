032c78742dae70d1230b4595ac3a334cfa32249a
ilozuluchris <ilozuluchidiuso@gmail.com>
2018-07-06 12:05:36 +0100

    Connecting hotel searrch view to template-  first draft

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,9 @@
-from .views import  book_flight
+from .views import  book_flight, hotel_search
 
 from django.conf.urls import url
 
 
 urlpatterns = [
     url('flight', book_flight, name='book_flight'),
-    url('hotel', book_flight, name='book_hotel'),
+    url('hotel', hotel_search, name='hotel_search'),
 ]
